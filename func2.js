0048997b5b0092641dc5b397ce22e69870e7fc63
ZaidAbuBader <zaid440@gmail.com>
2023-09-17 08:32:28 +0300

    update2

--- func2.js
+++ func2.js
@@ -1,15 +1,17 @@
 
-let mystudents = ["zaid", "naya", "tuta", "yara"]
-let mymarks = [44, 88, 33, 85]
+let mystudents = ["zaid", "naya", "tuta", "yara",99]
+let mymarks = [44, 88, 33, 85,"ahmad"]
 
 function markcalculate() {
 for (let i = 0; i < mymarks.length; i++) {
-    if (mymarks[i] >50) {
+    if (mymarks[i] >=50) {
     console.log("Congratulation " +"ya " + mystudents[i] + " , you success and your grade is  " + mymarks[i])}
 
-  else {
+  else if (mymarks[i] < 50) {
     console.log("Sorry " +"ya " + mystudents[i] + " ,  you failed and your grade is  " + mymarks[i])}
 
+    else{ 
+      console.log ("Please add a correct number")}
   }
     }
 
